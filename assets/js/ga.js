8320657c88e381d1e4f522188657f01626beb10c
yama5993 <yama5993@nate.com>
2025-12-23 14:51:49 +0900

    Google Analytics 설정 추가: gtag.js 스크립트 및 데이터 레이어 초기화 코드 삽입

---
+++ assets/js/ga.js
@@ -0,0 +1,8 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-KDKK3ZZK50', {
+	linker: {
+		domains: ['latindance.kr', 'archerlab.dev', 'itstory.archerlab.dev', 'walkwithme.archerlab.dev', 'golf.archerlab.dev', 'cupid.archerlab.dev']
+	}
+});
