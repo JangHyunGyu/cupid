46d6ec964052d79efc642e04670dd4adc2ceeeae
Ratenne / 김유진 <130076107+Ratenne@users.noreply.github.com>
2026-01-19 09:44:58 +0900

    보건교사 서비스컷 테스트 파일위치 수정

--- assets/js/scenario/ko_day2_4_night.js
+++ assets/js/scenario/ko_day2_4_night.js
@@ -75,7 +75,8 @@ Object.assign(SCENARIO[2], {
     "day2_night_nurse_home_4": {
         name: "보건선생님",
         text: "\"오늘 밤은... 조금 더 같이 있어줄래?\"",
-        character: "assets/images/event/nurse_home_event1.png",
+        background: "assets/images/background/nurse_home_event1.png",
+        character: null,
         night: true,
         next: "day2_night_nurse_home_5"
     },
