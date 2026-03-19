9cdcdcb1a28dbea1add673a5984dd4f39e9048fa
yama5993 <yama5993@nate.com>
2026-03-20 08:03:39 +0900

    에러 핸들러 노이즈 필터 추가 (외부 주입 스크립트 에러 무시)

--- assets/js/loaders/game-loader.js
+++ assets/js/loaders/game-loader.js
@@ -280,7 +280,16 @@
         } catch (_) { return ''; }
     }
 
+    function _isNoise(message, stack) {
+        if (!message) return true;
+        if (message === 'Script error.' && !stack) return true;
+        if (/Can't find variable: (gmo|__gCrWeb|ytcfg)/.test(message)) return true;
+        if (/ResizeObserver loop/.test(message)) return true;
+        return false;
+    }
+
     function _sendError(message, stack, url) {
+        if (_isNoise(message, stack)) return;
         var key = message + (url || '');
         if (key === _lastError) { _errorCount++; if (_errorCount > 3) return; }
         else { _lastError = key; _errorCount = 1; }
