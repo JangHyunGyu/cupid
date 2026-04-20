fab5fbfc39b1d47ba02857d7c9ce35a2c373bea4
yama5993 <yama5993@nate.com>
2026-04-20 22:20:59 +0900

    히스토리 사이즈 10개로 통일

--- assets/js/modules/StateManager.js
+++ assets/js/modules/StateManager.js
@@ -57,11 +57,6 @@ class StateManager {
         this.chatMemories = {};
 
         /**
-         * 캐릭터별 대화 누적 요약 (슬라이딩 윈도우 밖으로 밀려난 메시지의 요약본)
-         */
-        this.chatSummaries = {};
-
-        /**
          * 게임 플래그들 (이벤트 발생 여부 기록)
          * - 예: { metSeoyeon: true, knowsName_Seoyeon: true, ... }
          * - 시나리오의 condition/excludeCondition에서 참조됨
@@ -177,19 +172,6 @@ class StateManager {
      * @returns {Array} 대화 기록 (없으면 빈 배열)
      */
     getChatMemory(charName) { return this.chatMemories[charName] || []; }
-
-    /**
-     * 누적 대화 요약 저장
-     */
-    setChatSummary(charName, summary) {
-        if (!charName) return;
-        this.chatSummaries[charName] = summary || '';
-    }
-
-    /**
-     * 누적 대화 요약 조회
-     */
-    getChatSummary(charName) { return this.chatSummaries[charName] || ''; }
 
     /**
      * 전체 게임 상태를 객체로 내보내기 (저장용)
