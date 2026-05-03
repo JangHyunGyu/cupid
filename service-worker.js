6d1e5c18815be8b361ca68188151e6c9a1e63160
janghyungyu <yama5993@nate.com>
2026-05-04 00:33:14 +0900

    자산 버전 및 갤러리, 게임 관련 스크립트 버전 업데이트: 2.8.8 -> 2.8.9, 캐시 버전 업데이트: v3.3.0 -> v3.3.1

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * ============================================================================
  */
 
-const CACHE_VERSION = 'cupid-v3.3.0';
+const CACHE_VERSION = 'cupid-v3.3.1';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 const MEDIA_CACHE = CACHE_VERSION + '-media';
 
