892645496a33d0ba1ad947169f37d73571bc3269
janghyungyu <yama5993@nate.com>
2026-05-09 01:19:34 +0900

    버전 업데이트: 모든 HTML 파일에서 CSS 및 JS 파일의 버전을 2.8.9에서 2.9.3으로 변경하고, 서비스 워커의 캐시 버전을 3.3.3에서 3.3.4로 업데이트했습니다.

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * ============================================================================
  */
 
-const CACHE_VERSION = 'cupid-v3.3.3';
+const CACHE_VERSION = 'cupid-v3.3.4';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 const MEDIA_CACHE = CACHE_VERSION + '-media';
 
