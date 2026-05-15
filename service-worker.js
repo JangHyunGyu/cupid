5ae31718bc0b250bfe12ba96de0a60675b54180a
janghyungyu <yama5993@nate.com>
2026-05-16 06:35:27 +0900

    버전 업데이트: config.js, gallery-loader.js, game-loader.js, modules/config.js의 버전을 2.9.12에서 2.9.13으로 변경하고, 관련 HTML 파일에서 스크립트 버전 업데이트

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * ============================================================================
  */
 
-const CACHE_VERSION = 'cupid-v3.3.6';
+const CACHE_VERSION = 'cupid-v3.3.7';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 const MEDIA_CACHE = CACHE_VERSION + '-media';
 
