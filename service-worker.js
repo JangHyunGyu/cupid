d916fff953936939d178f5897d090715759d592b
janghyungyu <yama5993@nate.com>
2026-05-17 16:22:45 +0900

    Loosen mature free-talk romance prompts

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * ============================================================================
  */
 
-const CACHE_VERSION = 'cupid-v3.3.8';
+const CACHE_VERSION = 'cupid-v3.3.9';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 const MEDIA_CACHE = CACHE_VERSION + '-media';
 
