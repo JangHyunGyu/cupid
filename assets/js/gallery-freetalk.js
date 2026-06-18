90c019e663e0b9f05e7dd9667f118c657ca794ef
JangHyunGyu <yama5993@nate.com>
2026-06-18 11:27:08 +0900

    fix: 버전 업데이트 및 캐시 무효화를 위한 스크립트 수정

--- assets/js/gallery-freetalk.js
+++ assets/js/gallery-freetalk.js
@@ -16,7 +16,7 @@
  *   - window.GalleryFreeTalk
  */
 
-const GALLERY_FREETALK_PROMPT_VERSION = '2.7.2';
+const GALLERY_FREETALK_PROMPT_VERSION = '2.7.3';
 window.GALLERY_FREETALK_PROMPT_VERSION = GALLERY_FREETALK_PROMPT_VERSION;
 
 function normalizeGalleryPromptBlockForCache(content) {
--- assets/js/loaders/config.js
+++ assets/js/loaders/config.js
@@ -9,7 +9,7 @@
 
 const LoaderConfig = {
     // 버전 (캐시 버스팅용)
-    VERSION: '2.9.40',
+    VERSION: '2.9.41',
 
     // 기본 경로
     BASE_PATH: 'assets/js/',
