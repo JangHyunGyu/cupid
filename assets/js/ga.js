94242552e3bbd9882a829ed5000e07918059501a
JangHyunGyu <yama5993@nate.com>
2026-06-18 13:18:23 +0900

    Add Google Analytics engagement tracking script to multiple HTML files

--- assets/js/ga.js
+++ assets/js/ga.js
@@ -7,28 +7,10 @@ gtag('config', 'G-05YM7K3VX9', {
 	}
 });
 
-// 비주얼 노벨 참여시간 보정: 60초마다 engagement 이벤트 전송
-// GA4는 클릭/스크롤이 없으면 비활성으로 판정하므로 수동 핑 필요
-(function() {
-	var startTime = Date.now();
-	var _gaTimer = setInterval(function() {
-		if (document.visibilityState === 'visible') {
-			var elapsed = Math.round((Date.now() - startTime) / 1000);
-			gtag('event', 'vn_engagement', {
-				engagement_time_msec: 60000,
-				elapsed_seconds: elapsed
-			});
-		}
-	}, 60000);
-
-	function _gaClearTimer() { clearInterval(_gaTimer); }
-	window.addEventListener('pagehide', _gaClearTimer);
-
-	// 씬 전환 시 가상 페이지뷰 전송 (SPA 보정)
-	window.sendGAPageView = function(sceneName) {
-		gtag('event', 'page_view', {
-			page_title: 'Cupid - ' + sceneName,
-			page_location: window.location.href + '#' + sceneName
-		});
-	};
-})();
+// SPA virtual page view tracking
+window.sendGAPageView = function(sceneName) {
+	gtag('event', 'page_view', {
+		page_title: 'Cupid - ' + sceneName,
+		page_location: window.location.href + '#' + sceneName
+	});
+};
