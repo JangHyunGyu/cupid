a776f399652f36a6c57418db53b863dd313a647c
JangHyunGyu <yama5993@nate.com>
2026-07-09 17:33:05 +0900

    프리토킹 캐릭터 말투 강제 완화

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * ============================================================================
  */
 
-const CACHE_VERSION = 'cupid-v3.3.31';
+const CACHE_VERSION = 'cupid-v3.3.32';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 const MEDIA_CACHE = CACHE_VERSION + '-media';
 
