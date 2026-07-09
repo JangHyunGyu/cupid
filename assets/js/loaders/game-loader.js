9ad0787dde8debf84be9f63d8c3d151cea8b4420
JangHyunGyu <yama5993@nate.com>
2026-07-09 17:33:05 +0900

    프리토킹 캐릭터 말투 강제 완화

--- assets/js/loaders/game-loader.js
+++ assets/js/loaders/game-loader.js
@@ -53,7 +53,7 @@
      * 
      * 예: 2.2.0 → 2.2.1 또는 2.3.1
      */
-        const version = '2.9.63';
+        const version = '2.9.64';
     const LOAD_RETRIES = 3;
     const LOAD_TIMEOUT_MS = 15000;
 
