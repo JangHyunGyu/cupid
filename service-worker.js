e208a0fb864ab5976566c14b0cdd7f7e2c690b73
yama5993 <yama5993@nate.com>
2026-07-10 22:55:01 +0900

    Refactor code structure for improved readability and maintainability

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * ============================================================================
  */
 
-const CACHE_VERSION = 'cupid-v3.3.36';
+const CACHE_VERSION = 'cupid-v3.3.37';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 const MEDIA_CACHE = CACHE_VERSION + '-media';
 
