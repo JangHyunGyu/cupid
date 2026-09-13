dfe86c7be7ba85735f9a2a0ba4820236895c821f
yama5993 <yama5993@nate.com>
2026-09-13 17:13:47 +0900

    Balance Cupid title cast across portrait and landscape screens

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * ============================================================================
  */
 
-const CACHE_VERSION = 'cupid-v3.3.154';
+const CACHE_VERSION = 'cupid-v3.3.155';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 const MEDIA_CACHE = CACHE_VERSION + '-media';
 
