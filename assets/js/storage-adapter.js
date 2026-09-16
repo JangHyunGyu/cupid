55d3ff8d382ca00a6626287b897ded7e6d3f5464
장현규 <95130210+JangHyunGyu@users.noreply.github.com>
2026-09-16 18:56:24 +0900

    fix: prepend VV scroll guard in early-loaded storage-adapter

--- assets/js/storage-adapter.js
+++ assets/js/storage-adapter.js
@@ -1,3 +1,15 @@
+/* Prevent visualViewport scroll handlers from fighting streaming auto-scroll (화면 떨림). */
+(function () {
+  var vv = window.visualViewport;
+  if (!vv || vv.__archerScrollGuard) return;
+  vv.__archerScrollGuard = true;
+  var orig = vv.addEventListener.bind(vv);
+  vv.addEventListener = function (type, listener, options) {
+    if (type === 'scroll') return;
+    return orig(type, listener, options);
+  };
+})();
+
 (function installCupidStorage(root, createCupidStorageAdapter) {
     'use strict';
 
